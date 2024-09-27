2153aff11882df62435908fe99dd2d5a0434f490
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-27 15:24:20 -0400

    setup error handling

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 // --- Imports and Configuration ---
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const errorHandler = require('./middleware/errorHandler');
 const path = require("node:path");
+
+const app = express();
 
 // --- View Engine Setup ---
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // --- Error Handling Middleware ---
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(err.statusCode || 500).send(err.message);
-  });
+app.use(errorHandler);
 
 // --- Server Configuration and Startup ---
 const PORT = parseInt(process.env.USE_PORT, 10) || 3000;
